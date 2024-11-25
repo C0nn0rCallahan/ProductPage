86a610f979205b9a715a1a9ec43c1c019910c5b2
C0nn0rCallahan <cmc8041@psu.edu>
2024-11-24 20:45:18 -0500

    Add files via upload

---
+++ Database.js
@@ -0,0 +1,34 @@
+
+//product database
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://127.0.0.1:27017/storeDB";
+MongoClient.connect(url, function(err, client) {
+    if (err) throw err;  
+    const database = client.db('data');
+    const customers = database.createCollection('customer');
+    database.Collection("customer").insertMany(customerDatabase, function(err, res) {
+        if (err) throw err;
+        console.log("data added");
+        db.close();
+    });
+    const products = database.createCollection('products');
+    database.Collection("products").insertMany(productDataBase, function(err, res) {
+        if (err) throw err;
+        console.log("data added");
+        db.close();
+    });
+    const shoppingCart = database.createCollection('ShoppingCart');
+    database.Collection("ShoppingCart").insertMany(shoppingCart, function(err, res) {
+        if (err) throw err;
+        console.log("data added");
+        db.close();
+    });
+    const Shipping = database.createCollection('shipping');
+    database.Collection("shipping").insertMany(shippingDatabase, function(err, res) {
+        if (err) throw err;
+        console.log("data added");
+        db.close();
+    });
+});
+
+
